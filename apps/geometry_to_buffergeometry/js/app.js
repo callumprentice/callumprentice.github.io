e05328a9de0192a3c2371571b1e2f434359e7bc4
Callum Prentice <callum@gmail.com>
2015-05-22 17:07:11 -0700

    Added feature to switch geometry types

--- apps/geometry_to_buffergeometry/js/app.js
+++ apps/geometry_to_buffergeometry/js/app.js
@@ -2,15 +2,9 @@
  * THREE.Geometry to THREE.BufferGeometry Example
  * @author Callum Prentice / http://callum.com/
  */
-var camera, scene, renderer, controls, stats;
+var camera, scene, renderer, controls, stats, gui;
 var bufferMesh = 0;
-var density = 6;
-var randomHeights = true;
-var cylinderSides = 16;
-var endCaps = true;
-var maxHeight = 40;
-var topRadius = 0.0;
-var bottomRadius = 5.0;
+var parameters = 0;
 
 function app() {
     if (!Detector.webgl) {
@@ -47,7 +41,7 @@ function init() {
     scene.add(light2);
 
     controls = new THREE.TrackballControls(camera, renderer.domElement);
-    controls.minDistance =100.0;
+    controls.minDistance = 100.0;
     controls.maxDistance = 800.0;
     controls.dynamicDampingFactor = 0.1;
 
@@ -58,125 +52,253 @@ function init() {
 
     window.addEventListener('resize', onWindowResize, false);
 
-    var gui = new dat.GUI({
-        autoPlace: false,
-        preset: 'Spikes',
-        load: {
-            "closed": false,
-            "remembered": {
-                "Spikes": {
-                    "0": {
-                        "density": 66,
-                        "topRadius": 0,
-                        "bottomRadius": 6.1,
-                        "maxHeight": 65,
-                        "cylinderSides": 16,
-                        "endCaps": true,
-                        "randomHeights": true
-                    }
-                },
-                "Stubby": {
-                    "0": {
-                        "density": 40,
-                        "topRadius": 1.9508890770533447,
-                        "bottomRadius": 8.020321761219305,
-                        "maxHeight": 14.428619813717187,
-                        "cylinderSides": 4,
-                        "endCaps": true,
-                        "randomHeights": false
-                    }
-                },
-                "Golfball": {
-                    "0": {
-                        "density": 50,
-                        "topRadius": 1.9508890770533447,
-                        "bottomRadius": 6.06943268416596,
-                        "maxHeight": 1,
-                        "cylinderSides": 64,
-                        "endCaps": true,
-                        "randomHeights": false
-                    }
-                },
-                "Inward": {
-                    "0": {
-                        "density": 30,
-                        "topRadius": 6.719729043183743,
-                        "bottomRadius": 0,
-                        "maxHeight": 80,
-                        "cylinderSides": 16,
-                        "endCaps": false,
-                        "randomHeights": false
-                    }
-                },
-                "Tribble": {
-                    "0": {
-                        "density": 84,
-                        "topRadius": 0,
-                        "bottomRadius": 3.2514817950889077,
-                        "maxHeight": 60,
-                        "cylinderSides": 16,
-                        "endCaps": true,
-                        "randomHeights": false
-                    }
-                },
-                "Hexafloramethane": {
-                    "0": {
-                        "density": 36,
-                        "topRadius": 7.370025402201525,
-                        "bottomRadius": 7.4,
-                        "cylinderSides": 6,
-                        "maxHeight": 60,
-                        "endCaps": true,
-                        "randomHeights": true
-                    }
-                },
-                "Tubular Bells": {
-                    "0": {
-                        "density": 30,
-                        "topRadius": 6.719729043183743,
-                        "bottomRadius": 4.55207451312447,
-                        "cylinderSides": 64,
-                        "maxHeight": 80,
-                        "endCaps": false,
-                        "randomHeights": true
-                    }
-                },
-            },
-            "folders": {}
-        }
+    createGui('Cylinder');
+
+    createScene();
+}
+
+function createGeometry() {
+
+    if (parameters.geomType === 'Cylinder') {
+        return new THREE.CylinderGeometry(
+            parameters.radius, parameters.radius, parameters.maxHeight, parameters.numSides, 1, !parameters.endCaps);
+    } else
+    if (parameters.geomType === 'Cone') {
+        return new THREE.CylinderGeometry(
+            parameters.radius1, parameters.radius2, parameters.maxHeight, parameters.numSides, 1, !parameters.endCaps);
+    } else
+    if (parameters.geomType === 'Sphere') {
+        return new THREE.SphereGeometry(
+            parameters.radius, parameters.segments, parameters.segments);
+    } else
+    if (parameters.geomType === 'Tetrahedron') {
+        return new THREE.TetrahedronGeometry(
+            parameters.radius);
+    } else
+    if (parameters.geomType === 'Heart') {
+        var heartShape = new THREE.Shape(); // From http://blog.burlock.org/html5/130-paths
+        var x = 0,
+            y = 0;
+        heartShape.moveTo(x + 25, y + 25);
+        heartShape.bezierCurveTo(x + 25, y + 25, x + 20, y, x, y);
+        heartShape.bezierCurveTo(x - 30, y, x - 30, y + 35, x - 30, y + 35);
+        heartShape.bezierCurveTo(x - 30, y + 55, x - 10, y + 77, x + 25, y + 95);
+        heartShape.bezierCurveTo(x + 60, y + 77, x + 80, y + 55, x + 80, y + 35);
+        heartShape.bezierCurveTo(x + 80, y + 35, x + 80, y, x + 50, y);
+        heartShape.bezierCurveTo(x + 35, y, x + 25, y + 25, x + 25, y + 25);
+        var extrudeSettings = {
+            amount: 8,
+            bevelEnabled: true,
+            bevelSegments: 1,
+            steps: 2,
+            bevelSize: 1,
+            bevelThickness: 1
+        };
+        var geom = new THREE.ExtrudeGeometry(heartShape, extrudeSettings);
+        geom.applyMatrix(new THREE.Matrix4().makeRotationX(-THREE.Math.degToRad(90)));
+        geom.applyMatrix(new THREE.Matrix4().makeScale(0.1, 0.1, 0.1));
+        geom.applyMatrix(new THREE.Matrix4().makeScale(parameters.radius, parameters.radius, parameters.radius));
+        return geom;
+    } else
+    if (parameters.geomType === 'Torus') {
+        return new THREE.TorusGeometry(
+            parameters.radius1, parameters.radius2, parameters.segments, parameters.segments);
+    } else
+    if (parameters.geomType === 'Torus Knot') {
+        return new THREE.TorusKnotGeometry(
+            parameters.radius1, parameters.radius2, parameters.segments, parameters.segments);
+    }
+}
+
+function createGui(geom_type) {
+
+    if (gui && gui.domElement) {
+        document.body.removeChild(gui.domElement)
+    }
+
+    gui = new dat.GUI({
+        autoPlace: false
     });
 
-    gui.remember(this);
     gui.domElement.style.position = 'absolute';
     gui.domElement.style.top = '24px';
     gui.domElement.style.right = '0';
     document.body.appendChild(gui.domElement);
-    gui.add(this, 'density', 1, 180).step(2).name("Density").listen().onFinishChange(function (value) {
-        addCones();
-    });
-    gui.add(this, 'topRadius', 0, 20).name("Top radius").listen().onFinishChange(function (value) {
-        addCones();
-    });
-    gui.add(this, 'bottomRadius', 0, 20).name("Bottom radius").listen().onFinishChange(function (value) {
-        addCones();
-    });
-    gui.add(this, 'cylinderSides', 3, 64).step(1).name("Cylinder sides").listen().onFinishChange(function (value) {
-        addCones();
-    });
-    gui.add(this, 'maxHeight', 1, 80).name("Max height").listen().onFinishChange(function (value) {
-        addCones();
-    });
-    gui.add(this, 'endCaps', true).name("End caps").onChange(function (value) {
-        addCones();
-    });
-    gui.add(this, 'randomHeights', true).name("Random heights").onChange(function (value) {
-        addCones();
-    });
 
-    addCones();
+    parameters = {
+        density: 50,
+        radius1: 10,
+        radius2: 5,
+        radius: 8,
+        numSides: 16,
+        maxHeight: 40,
+        endCaps: true,
+        segments: 16,
+        randomHeights: true
+    };
+
+    var object_list = ['Cylinder', 'Cone', 'Sphere', 'Tetrahedron', 'Heart', 'Torus', 'Torus Knot'];
+
+    if (geom_type === 'Cylinder') {
+
+        parameters.geomType = geom_type;
+
+        gui.add(parameters, 'geomType', object_list).name('Geometry type').onChange(function (value) {
+            createGui(value);
+            createScene();
+        });
+        gui.add(parameters, 'density', 1, 180).step(2).name("Density").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'radius', 0, 20).name("Radius").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'numSides', 3, 64).step(1).name("Cylinder sides").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'maxHeight', 1, 80).name("Max height").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'endCaps', true).name("End caps").onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'randomHeights', true).name("Random heights").onChange(function (value) {
+            createScene();
+        });
+    } else
+    if (geom_type === 'Cone') {
+
+        parameters.geomType = geom_type;
+        parameters.radius1 = 0;
+        parameters.radius2 = 9;
+        parameters.endCaps = false;
+
+        gui.add(parameters, 'geomType', object_list).name('Geometry type').onChange(function (value) {
+            createGui(value);
+            createScene();
+        });
+        gui.add(parameters, 'density', 1, 180).step(2).name("Density").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'radius1', 0, 20).name("Top radius").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'radius2', 0, 20).name("Bottom radius").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'numSides', 3, 64).step(1).name("Cone sides").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'maxHeight', 1, 80).name("Max height").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'endCaps', true).name("End caps").onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'randomHeights', true).name("Random heights").onChange(function (value) {
+            createScene();
+        });
+    } else
+    if (geom_type === 'Sphere') {
+
+        parameters.geomType = geom_type;
+        parameters.density = 30;
+        parameters.radius = 12;
+        parameters.maxHeight = 50.0;
+        parameters.segments = 16;
+        parameters.randomHeights = false;
+
+        gui.add(parameters, 'geomType', object_list).name('Geometry type').onChange(function (value) {
+            createGui(value);
+            createScene();
+        });
+        gui.add(parameters, 'density', 1, 180).step(2).name("Density").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'radius', 0, 20).name("Radius").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'segments', 0, 20).step(1).name("Segments").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'randomHeights', true).name("Randomize position").onChange(function (value) {
+            createScene();
+        });
+    } else
+    if (geom_type === 'Tetrahedron') {
+
+        parameters.geomType = geom_type;
+        parameters.density = 46;
+        parameters.radius = 15;
+        parameters.maxHeight = 50.0;
+        parameters.randomHeights = true;
+
+        gui.add(parameters, 'geomType', object_list).name('Geometry type').onChange(function (value) {
+            createGui(value);
+            createScene();
+        });
+        gui.add(parameters, 'density', 1, 180).step(2).name("Density").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'radius', 0, 20).name("Radius").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'randomHeights', true).name("Randomize position").onChange(function (value) {
+            createScene();
+        });
+    } else
+    if (geom_type === 'Heart') {
+
+        parameters.geomType = geom_type;
+        parameters.density = 20;
+        parameters.radius = 3.5;
+        parameters.maxHeight = 50.0;
+        parameters.randomHeights = false;
+
+        gui.add(parameters, 'geomType', object_list).name('Geometry type').onChange(function (value) {
+            createGui(value);
+            createScene();
+        });
+        gui.add(parameters, 'density', 1, 180).step(2).name("Density").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'radius', 0, 20).name("Radius").listen().onFinishChange(function (value) {
+            createScene();
+        });
+    } else
+    if (geom_type === 'Torus' || geom_type === 'Torus Knot') {
+
+        parameters.geomType = geom_type;
+        parameters.density = 24;
+        parameters.radius1 = 16;
+        parameters.radius2 = 2;
+        parameters.maxHeight = 50.0;
+        parameters.segments = 32;
+        parameters.randomHeights = false;
+
+        gui.add(parameters, 'geomType', object_list).name('Geometry type').onChange(function (value) {
+            createGui(value);
+            createScene();
+        });
+        gui.add(parameters, 'density', 1, 180).step(2).name("Density").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'radius1', 0, 20).name("Radius").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'radius2', 0, 20).name("Tube").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'segments', 0, 64).step(1).name("Segments").listen().onFinishChange(function (value) {
+            createScene();
+        });
+        gui.add(parameters, 'randomHeights', true).name("Randomize position").onChange(function (value) {
+            createScene();
+        });
+    }
 }
 
-function addCones() {
+function createScene() {
 
     if (bufferMesh) {
         scene.remove(bufferMesh);
@@ -188,21 +310,21 @@ function addCones() {
     var normals = 0;
     var colors = 0;
 
-    for (var num_lat = 0; num_lat < density / 2; ++num_lat) {
+    for (var num_lat = 0; num_lat < parameters.density / 2; ++num_lat) {
 
-        var lhs = (num_lat + 0) * 180 / (density/2);
-        var rhs = (num_lat + 1) * 180 / (density/2);
-        var lat = (lhs + rhs) / 2.0;
+        var lat_lhs = (num_lat + 0) * 180 / (parameters.density / 2);
+        var lat_rhs = (num_lat + 1) * 180 / (parameters.density / 2);
+        var lat = (lat_lhs + lat_rhs) / 2.0;
 
-        for (var num_lng = 0; num_lng < density; ++num_lng) {
+        for (var num_lng = 0; num_lng < parameters.density; ++num_lng) {
 
-            var lhs = (num_lng + 0) * 360 / density;
-            var rhs = (num_lng + 1) * 360 / density;
-            var lng = (lhs + rhs) / 2.0;
+            var lng_lhs = (num_lng + 0) * 360 / parameters.density;
+            var lng_rhs = (num_lng + 1) * 360 / parameters.density;
+            var lng = (lng_lhs + lng_rhs) / 2.0;
 
-            var height = maxHeight;
-            if (randomHeights)
-                height = Math.random() * ( maxHeight - 5.0) + 5;
+            var height = parameters.maxHeight;
+            if (parameters.randomHeights)
+                height = Math.random() * (parameters.maxHeight - 5.0) + 5;
 
             var phi = lat * Math.PI / 180.0;
             var theta = lng * Math.PI / 180.0;
@@ -210,7 +332,8 @@ function addCones() {
             var y = radius * Math.cos(phi);
             var z = radius * Math.sin(phi) * Math.sin(theta);
 
-            var geometry = new THREE.CylinderGeometry(topRadius, bottomRadius, height, cylinderSides, 1, !endCaps);
+            var geometry = createGeometry();
+
             geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, height / 2, 0));
             geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-THREE.Math.degToRad(90)));
             geometry.applyMatrix(new THREE.Matrix4().lookAt(new THREE.Vector3(0, 0, 0), new THREE.Vector3(x, y, z), new THREE.Vector3(0, 1, 0)));
@@ -220,9 +343,9 @@ function addCones() {
             color.setHSL(lat / 180.0, 1.0, 0.7);
 
             if (positions === 0) {
-                var num_stacks = density * density / 2;
+                var num_stacks = parameters.density * parameters.density / 2;
 
-                var str = "Geometry to BufferGeometry Example - " + parseInt(num_stacks * geometry.faces.length) + " triangles";
+                var str = "Geometry to BufferGeometry Example - " + parseInt(num_stacks * geometry.faces.length, 10) + " triangles";
                 document.getElementById('title').textContent = str.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
                 positions = new Float32Array(num_stacks * geometry.faces.length * 3 * 3);
@@ -232,7 +355,7 @@ function addCones() {
 
             geometry.faces.forEach(function (face, index) {
 
-                var cur_element = ((num_lng + num_lat * density) * geometry.faces.length + index);
+                var cur_element = ((num_lng + num_lat * parameters.density) * geometry.faces.length + index);
 
                 positions[cur_element * 9 + 0] = geometry.vertices[face.a].x;
                 positions[cur_element * 9 + 1] = geometry.vertices[face.a].y;
